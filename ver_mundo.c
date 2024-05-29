053157b644368b91f1a16ad6f6a1e9aa7edb427f
Luís Soares <lsoares@newstore.com>
2024-05-29 18:10:05 +0100

    clean

--- ver_mundo.c
+++ ver_mundo.c
@@ -10,23 +10,35 @@ void desenhaRetangulo(char mundo[][LARGURA_MUNDO + 1], Retangulo retangulo)
         for (int xx = retangulo.x; xx < retangulo.x + retangulo.l; xx++)
             mundo[yy][xx] = isPontoContorno(retangulo, xx, yy) ? PINTADO_CONTORNO : PINTADO_INTERIOR;
 }
-
-void imprimeMundo(Retangulos *retangulos)
+void desenhaRetangulos(Retangulos *retangulos, char mundo[][81])
 {
-    char mundo[ALTURA_MUNDO + 1][LARGURA_MUNDO + 1]; // extra para referencial em 1,1 e evitar contas
-    memset(mundo, VAZIO, sizeof(mundo));             // limpa
-    for (int i = 0; i < retangulos->total; i++)      // desenha retangulos
+    for (int i = 0; i < retangulos->total; i++)
         desenhaRetangulo(mundo, retangulos->lista[i]);
-    for (int x = 1; x <= LARGURA_MUNDO; x++)
-        printf(x % 10 == 0 ? "%d" : "-", x / 10);
-    printf("\n");
+}
+
+void imprimeQuadro(char mundo[][81])
+{
     for (int y = ALTURA_MUNDO; y >= 1; y--) // imprime
     {
         for (int x = 1; x <= LARGURA_MUNDO; x++)
             printf("%c", mundo[y][x]);
         printf("\n");
     }
+}
+
+void imprimeGuia()
+{
     for (int x = 1; x <= LARGURA_MUNDO; x++)
         printf(x % 10 == 0 ? "%d" : "-", x / 10);
     printf("\n");
+}
+
+void imprimeMundo(Retangulos *retangulos)
+{
+    char mundo[ALTURA_MUNDO + 1][LARGURA_MUNDO + 1]; // extra para referencial em 1,1 e evitar contas
+    memset(mundo, VAZIO, sizeof(mundo));
+    desenhaRetangulos(retangulos, mundo);
+    imprimeGuia();
+    imprimeQuadro(mundo);
+    imprimeGuia();
 }
