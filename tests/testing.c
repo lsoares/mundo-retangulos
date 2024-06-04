d070b54dc5f765f068500c964f2cd0949bbead46
Luís Soares <lsoares@newstore.com>
2024-06-04 14:14:37 +0100

    assert

--- tests/testing.c
+++ tests/testing.c
@@ -13,7 +13,9 @@ int runCommand(const char *executable, char *output)
     char row[1000];
     while (fgets(row, sizeof(row), pipe))
     {
-        assert(strlen(output) + strlen(row) < MAX_OUTPUT_LEN);
+        printf("sizeof(row): %lu", sizeof(row));
+        assert(strlen(row) < sizeof(row)); // fails on larger rows. need to increase their size above
+        assert(strlen(output) + strlen(row) < MAX_OUTPUT_LEN); // fail on output size not enough
         strcat(output, row);
     }
     int exitStatus = pclose(pipe);
@@ -23,7 +25,8 @@ int runCommand(const char *executable, char *output)
 
 int pipeToRunCommand(const char *input, const char *command, char *output)
 {
-    char fullCommand[2048];
+    char fullCommand[2020];
+    assert(strlen(command) + strlen(input) < 2000);  // fails if we need a bigger command length
     snprintf(fullCommand, sizeof(fullCommand), "echo '%s' | %s 2>&1", input, command); // redirect stderr to stdout
     return runCommand(fullCommand, output);
 }
