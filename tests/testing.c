05a0f0ae63a619506f5b00d64cdb60c42626a720
Luís Soares <lsoares@newstore.com>
2024-06-07 18:59:29 +0100

    simpl.

--- tests/testing.c
+++ tests/testing.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <stdbool.h>
 #include "testing.h"
 
 int runCommand(const char *executable, char *output)
--- tests/testing.h
+++ tests/testing.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-#include <assert.h>
 #include <stdbool.h>
 
 #define MAX_OUTPUT_LEN 15000
